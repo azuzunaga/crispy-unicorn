2a9fbba75e020c913bd3b884a5dbcbd10514da20
Americo <ame.zuz@gmail.com>
2018-06-04 16:56:57 -0400

    Ammend users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->integer('fav_color_id')->unique();
             $table->timestamps();
         });
     }
