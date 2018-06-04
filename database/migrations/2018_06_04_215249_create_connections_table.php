66ec6fce69217bce79bc27dfb716606fdbead072
Americo <ame.zuz@gmail.com>
2018-06-04 17:54:21 -0400

    Create Connection model, controller, and migration

---
+++ database/migrations/2018_06_04_215249_create_connections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConnectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('connections', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('smaller_id')->unsigned();
+            $table->foreign('smaller_id')->references('id')->on('users');
+            $table->integer('bigger_id')->unsigned();
+            $table->foreign('bigger_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('connections');
+    }
+}
