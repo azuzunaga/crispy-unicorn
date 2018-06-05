f71817cfc653061e2928daf035d0bc988f5b1f99
Americo <ame.zuz@gmail.com>
2018-06-05 14:22:02 -0400

    Add belongsTo associations

--- app/Connection.php
+++ app/Connection.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Connection extends Model
 {
-    //
+    public function small()
+    {
+        return $this->belongsTo(User::class, 'smaller_id');
+    }
+
+    public function big()
+    {
+        return $this->belongsTo(User::class, 'bigger_id');
+    }
 }
