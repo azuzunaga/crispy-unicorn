e39785e1b1b539449b34ab5e18a79a23f431b797
Americo <ame.zuz@gmail.com>
2018-06-04 23:33:26 -0400

    Define relationship between users and colors

--- app/Color.php
+++ app/Color.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Color extends Model
 {
-    //
+    public function likers()
+    {
+        return $this->hasMany(User::class, 'fav_color_id');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    public function favoriteColor()
+    {
+        return $this->belongsTo(Color::class, 'fav_color_id');
+    }
 }
