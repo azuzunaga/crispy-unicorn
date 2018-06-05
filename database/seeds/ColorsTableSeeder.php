d9e6c4c622634cec6fd379d2553f0c0adbe5e11d
Americo <ame.zuz@gmail.com>
2018-06-04 20:27:38 -0400

    Create user factory, add created and updated field to colors seed

--- database/seeds/ColorsTableSeeder.php
+++ database/seeds/ColorsTableSeeder.php
@@ -11,18 +11,18 @@ class ColorsTableSeeder extends Seeder
 
       //insert some dummy records
       DB::table('colors')->insert(array(
-        array('color_name'=>'red', 'color_code'=>'#FF0000'),
-        array('color_name'=>'orange', 'color_code'=>'#FF8000'),
-        array('color_name'=>'yellow', 'color_code'=>'#FFFF00'),
-        array('color_name'=>'chartreuse', 'color_code'=>'#80FF00'),
-        array('color_name'=>'green', 'color_code'=>'#00FF00'),
-        array('color_name'=>'spring green', 'color_code'=>'#00FF80'),
-        array('color_name'=>'cyan', 'color_code'=>'#00FFFF'),
-        array('color_name'=>'azure', 'color_code'=>'#0080FF'),
-        array('color_name'=>'blue', 'color_code'=>'#0000FF'),
-        array('color_name'=>'violet', 'color_code'=>'#8000FF'),
-        array('color_name'=>'magenta', 'color_code'=>'#FF00FF'),
-        array('color_name'=>'rose', 'color_code'=>'#FF0080'),
+        array('color_name'=>'red', 'color_code'=>'#FF0000', 'created_at'=>NOW(), 'updated_at'=>NOW()),
+        array('color_name'=>'orange', 'color_code'=>'#FF8000', 'created_at'=>NOW(), 'updated_at'=>NOW()),
+        array('color_name'=>'yellow', 'color_code'=>'#FFFF00', 'created_at'=>NOW(), 'updated_at'=>NOW()),
+        array('color_name'=>'chartreuse', 'color_code'=>'#80FF00', 'created_at'=>NOW(), 'updated_at'=>NOW()),
+        array('color_name'=>'green', 'color_code'=>'#00FF00', 'created_at'=>NOW(), 'updated_at'=>NOW()),
+        array('color_name'=>'spring green', 'color_code'=>'#00FF80', 'created_at'=>NOW(), 'updated_at'=>NOW()),
+        array('color_name'=>'cyan', 'color_code'=>'#00FFFF', 'created_at'=>NOW(), 'updated_at'=>NOW()),
+        array('color_name'=>'azure', 'color_code'=>'#0080FF', 'created_at'=>NOW(), 'updated_at'=>NOW()),
+        array('color_name'=>'blue', 'color_code'=>'#0000FF', 'created_at'=>NOW(), 'updated_at'=>NOW()),
+        array('color_name'=>'violet', 'color_code'=>'#8000FF', 'created_at'=>NOW(), 'updated_at'=>NOW()),
+        array('color_name'=>'magenta', 'color_code'=>'#FF00FF', 'created_at'=>NOW(), 'updated_at'=>NOW()),
+        array('color_name'=>'rose', 'color_code'=>'#FF0080', 'created_at'=>NOW(), 'updated_at'=>NOW()),
      ));
     }
 }
