4363442d573c3115ccb75898f3e3cff7f0714c05
Americo <ame.zuz@gmail.com>
2018-06-05 22:30:20 -0400

    Remove time data

--- frontend/app/models/color.js
+++ frontend/app/models/color.js
@@ -3,6 +3,4 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   code: DS.attr('string'),
-  createdAt: DS.attr('string'),
-  updatedAt: DS.attr('string')
 });
